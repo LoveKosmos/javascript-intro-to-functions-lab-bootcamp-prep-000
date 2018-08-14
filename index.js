6ef554cc91ff82226210347a9a5406215aaef123
LoveKosmos <nataliiariabkova@gmail.com>
2018-08-14 02:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function shout(string){
   return string.toUpperCase()
 }
-function whisper(string){
-  return string.toLowerCase()
+function whisper(string1){
+  return string1.toLowerCase()
 }
-function logShout(string){
-  console.log(string.toUpperCase())
+function logShout(string2){
+  console.log(string2.toUpperCase())
 }
-function logWhisper(string){
-  console.log(string.toLowerCase())
+function logWhisper(string3){
+  console.log(string3.toLowerCase())
 }
 
   
